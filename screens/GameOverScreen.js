7fe08e1dadda69c1e898a219e8cd5e157d033dbb
Eric Ho <endonoho@gmail.com>
2021-10-12 19:04:15 -0700

    Restart the game

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -5,6 +5,9 @@ const GameOverScreen = props => {
 	return (
 		<View style={styles.screen}>
 			<Text>The Game is Over!</Text>
+			<Text>Number of rounds: {props.roundsNumber}</Text>
+			<Text>Number was: {props.userNumber}</Text>
+			<Button title="NEW GAME" onPress={props.onRestart}></Button>
 		</View>
 	)
 }
