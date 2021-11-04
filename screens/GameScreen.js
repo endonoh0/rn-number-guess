55688e69f4c120aa7962eb3eceef9209e1decff7
Eric Ho <endonoho@gmail.com>
2021-11-03 20:11:29 -0700

    Update scroll view to grow

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -74,8 +74,8 @@ const GameScreen = props => {
 					<Ionicons name="md-add" size={24} color="white" />
 				</MainButton>
 			</Card>
-			<View style={styles.list}>
-				<ScrollView>
+			<View style={styles.listContainer}>
+				<ScrollView contentContainerStyle={styles.list}>
 					{pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
 				</ScrollView>
 			</View>
@@ -96,9 +96,14 @@ const styles = StyleSheet.create({
 		width: 400,
 		maxWidth: '90%'
 	},
-	list: {
+	listContainer: {
 		flex: 1,
 		width: '80%'
+	},
+	list: {
+		flexGrow: 1,
+		alignItems: 'center',
+		justifyContent: 'flex-end'
 	},
 	listItem: {
 		borderColor: '#ccc',
@@ -107,7 +112,8 @@ const styles = StyleSheet.create({
 		marginVertical: 10,
 		backgroundColor: 'white',
 		flexDirection: 'row',
-		justifyContent: 'space-around'
+		justifyContent: 'space-around',
+		width: '60%'
 	}
 });
 
