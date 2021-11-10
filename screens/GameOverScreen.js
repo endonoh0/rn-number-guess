1812ad50802c15c1819bafa18740ad724408e0a5
Eric Ho <endonoho@gmail.com>
2021-11-09 20:36:36 -0800

    Calculate styles dynamically

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Button, Image, Text } from 'react-native';
+import { View, StyleSheet, Image, Text, Dimensions, ScrollView } from 'react-native';
 
 import Colors from '../constants/colors';
 import BodyText from '../components/BodyText';
@@ -9,27 +9,30 @@ import MainButton from '../components/MainButton';
 const GameOverScreen = props => {
 	const {roundsNumber, userNumber, onRestart} = props;
 	return (
-		<View style={styles.screen}>
-			<TitleText>The Game is Over!</TitleText>
-			<View style={styles.imageContainer}>
-				<Image
-					fadeDuration={1000}
-					// source={{uri: 'https://http.cat/503'}}
-					source={require('../assets/success.png')}
-					style={styles.image}
-					resizeMode="cover"
-				/>
+		<ScrollView>
+			<View style={styles.screen}>
+				<TitleText>The Game is Over!</TitleText>
+				<View style={styles.imageContainer}>
+					<Image
+						fadeDuration={1000}
+						// source={{uri: 'https://http.cat/503'}}
+						source={require('../assets/success.png')}
+						style={styles.image}
+						resizeMode="cover"
+					/>
+				</View>
+				<View style={styles.resultContainer}>
+					<BodyText style={styles.resultText}>
+						Your phone needed {' '}
+						<Text style={styles.highlight}>{roundsNumber}</Text> {' '}
+						to guess the number {' '}
+						<Text style={styles.highlight}>{userNumber}</Text>.
+					</BodyText>
+				</View>
+				<MainButton onPress={onRestart}>NEW GAME</MainButton>
 			</View>
-			<View style={styles.resultContainer}>
-				<BodyText style={styles.resultText}>
-					Your phone needed {' '}
-					<Text style={styles.highlight}>{roundsNumber}</Text> {' '}
-					to guess the number {' '}
-					<Text style={styles.highlight}>{userNumber}</Text>.
-				</BodyText>
-			</View>
-			<MainButton onPress={onRestart}>NEW GAME</MainButton>
-		</View>
+		</ScrollView>
+
 	)
 }
 
@@ -40,25 +43,25 @@ const styles = StyleSheet.create({
 		alignItems: 'center'
 	},
 	imageContainer: {
-		width: 300,
-		height: 300,
-		borderRadius: 150,
+		width: Dimensions.get('window').width * 0.7,
+		height: Dimensions.get('window').width * 0.7,
+		borderRadius: Dimensions.get('window').width * 0.7 / 2,
 		borderWidth: 3,
 		borderColor: 'black',
 		overflow: 'hidden',
-		marginVertical: 30
+		marginVertical: Dimensions.get('window').height / 30
 	},
 	image: {
 		width: '100%',
 		height: '100%'
 	},
 	resultContainer: {
-		marginHorizontal: 20,
-		marginVertical: 15
+		marginHorizontal: 30,
+		marginVertical: Dimensions.get('window').height / 60
 	},
 	resultText: {
 		textAlign: 'center',
-		fontSize: 20
+		fontSize: Dimensions.get('window').height < 400 ? 16 : 20
 	},
 	highlight: {
 		color: Colors.primary,
