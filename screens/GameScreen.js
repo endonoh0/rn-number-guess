d93f37ada21d61a18a99ae86b26c826ddc5fc54e
Eric Ho <endonoho@gmail.com>
2021-11-09 20:20:13 -0800

    Set height dimensions

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { View, Text, StyleSheet, Alert, ScrollView, FlatList } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView, FlatList, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import Card from '../components/Card';
@@ -98,7 +98,8 @@ const styles = StyleSheet.create({
 	buttonContainer: {
 		flexDirection: 'row',
 		justifyContent: 'space-around',
-		marginTop: 20,
+		// marginTop: 20,
+		marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
 		width: 400,
 		maxWidth: '90%'
 	},
